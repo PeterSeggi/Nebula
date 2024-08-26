3fe382c482075a4fe2c2b32dbd0dc81340cc8912
Peter Seggi <pedro.seggiaro@gmail.com>
2024-08-26 12:39:51 -0300

    Tailwind finally works!

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
     return (
         <div>
-            <h1>Hello World! 🪐</h1>
+            <h1 className="font-bold text-blue-400">Hello World! 🪐</h1>
         </div>
     )
 }
