9fa1608a6957910fa1ab4e007efaf538eb5f6837
Peter Seggi <pedro.seggiaro@gmail.com>
2024-08-26 12:22:42 -0300

    React fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World! 🪐</h1>
-      <p>Welcome to your Electron application.</p>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <h1>Hello World! 🪐</h1>
+        </div>
+    )
 }
-
-export default App;
--- src/renderer.js
+++ src/renderer.js
@@ -27,6 +27,6 @@
  */
 
 import './index.css';
-import './index.js';
+import './index.jsx';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
