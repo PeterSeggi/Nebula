7b73d2e8543e9572a9d98421c98dc23253770219
Peter Seggi <pedro.seggiaro@gmail.com>
2024-08-27 18:50:08 -0300

    Creamos el canvas element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Canvas from "./components/Canvas.jsx";
 
 export default function App() {
     return (
-        <div className="bg-neblack h-screen w-screen">
-            <h1 className="font-bold text-almond text-4xl">Hello World! 🪐</h1>
+        <div className="bg-neblack h-screen w-screen p-4">
+            <h1 className="bg-gradient-to-r from-gunmetal font-bold text-almond text-4xl mb-2 p-1">Hello Nebula 🪐</h1>
+            <Canvas/>
         </div>
     )
 }
---
+++ src/components/Canvas.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Canvas(){
+    return(
+        <div className="bg-gunmetal">
+            <input className="bg-chestnut border-0 h-fit" type="text" />
+        </div>
+    )
+}
+
+export default Canvas;
